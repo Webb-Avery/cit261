293ac3f759a36087e600528481a0fedeed7844cb
Avery Webb <averymwebb@gmail.com>
2017-10-11 00:07:32 -0600

    Add files via upload

---
+++ CSS_FUN/css_fun.js
@@ -0,0 +1,15 @@
+function makeBlue() 
+{
+    document.getElementById("toEdit").style.color = "blue";
+}
+
+function backgroundGreen()
+{
+    document.body.style.background = "green";
+}
+
+function reset()
+{
+    document.getElementById("toEdit").style.color = "black";
+    document.body.style.background = "azure";
+}
