6f0bfe71d3a5d595a2866e9b854d4bb5e47328b2
Avery Webb <averymwebb@gmail.com>
2017-10-11 17:37:34 -0600

    Update css_fun.js

--- CSS/css_fun.js
+++ CSS/css_fun.js
@@ -8,8 +8,51 @@ function backgroundGreen()
     document.body.style.background = "green";
 }
 
+function centerText()
+{
+    document.getElementById("toEdit").style.textAlign = "center";
+}
+
+function backgroundChoice()
+{
+    var x = document.getElementById("background").value;
+    document.body.style.background = x;
+}
+
+function textChoice()
+{
+    var x = document.getElementById("text").value;
+    document.getElementById("toEdit").style.color = x;
+}
+
+function buttonChoice()
+{
+    var x = document.getElementById("buttonColor").value;
+    document.getElementById("button1").style.backgroundColor = x;
+    document.getElementById("button2").style.backgroundColor = x;
+    document.getElementById("button3").style.backgroundColor = x;
+    document.getElementById("button4").style.backgroundColor = x;
+    document.getElementById("button5").style.backgroundColor = x;
+    document.getElementById("button6").style.backgroundColor = x;
+    document.getElementById("button7").style.backgroundColor = x;
+    document.getElementById("button8").style.backgroundColor = x;
+}
+
 function reset()
 {
     document.getElementById("toEdit").style.color = "black";
     document.body.style.background = "azure";
+    document.getElementById("toEdit").style.textAlign = "left";
+}
+
+function behindCurtain()
+{
+    if (document.getElementById("behindCurtain").style.visibility == "hidden")
+    {
+        document.getElementById("behindCurtain").style.visibility = "visible";
+    }
+    else
+    {
+        document.getElementById("behindCurtain").style.visibility = "hidden";
+    }
 }
